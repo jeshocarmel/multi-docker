db992c49a91666f8238b7e612578a3d68ecf1803
jeshocarmel <jeshocarmel@gmail.com>
2019-12-05 16:41:50 +0800

    initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () =>{
+
+    return(
+        <div>
+            Im some other pager 
+            <Link to="/">Go back to home page!</Link>
+        </div>
+    );
+};
